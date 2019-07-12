02d241225459502cc6ef34d2e93808704272c4a9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-12 11:21:38 -0400

    Adds WHILE loop to 'proof_of_work()' method

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -23,7 +23,8 @@ def proof_of_work(last_proof):
 
     print("Searching for next proof")
     proof = 0
-    #  TODO: Your code here
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
