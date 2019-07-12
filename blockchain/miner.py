94ef9788385750afe4162bd20cdd7eb0ba369136
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-07-12 11:40:44 -0400

    Changes 'proof' start value to 50

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -22,8 +22,9 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
+    proof = 50
     while valid_proof(last_proof, proof) is False:
+        # print(proof)
         proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
@@ -39,8 +40,11 @@ def valid_proof(last_hash, proof):
     """
 
     guess = f'{last_hash}{proof}'.encode()
-    guess_hash = hashlib.sha256(guess).hexdigest()
-    return guess_hash[:6] == proof[6:]
+    new_hash = hashlib.sha256(guess).hexdigest()
+    # print("new_hash: ", new_hash)
+    # print("last_hash: ", last_hash)
+    # return guess_hash[-6:] == str(proof)[:6]
+    return str(last_hash)[-6:] == str(new_hash)[:6]
 
 if __name__ == '__main__':
     # What node are we interacting with?
